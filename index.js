d8efa996c7e7c3373a37a3efd7b8680c1944da54
Michael <mkenton2020@icloud.com>
2021-07-22 10:56:08 -0500

    Completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+const { arrayContaining } = require("expect")
+
+function findMatching(arr, string) {
+    string = string.toUpperCase()
+    return arr.filter(function (x) {return x.toUpperCase() === string})
+}
+
+function fuzzyMatch(arr, prefix) {
+    return arr.filter(el => el.startsWith(prefix))
+}
+   
+function matchName(arr,string) {
+    return arr.filter(el => el.name === string)
+}
